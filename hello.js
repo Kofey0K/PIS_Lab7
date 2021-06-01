dc4424a8f6aa927e403e21f8aa8bdbf3f3313eb6
Kofey0K <56653351+Kofey0K@users.noreply.github.com>
2021-06-01 19:26:38 +0300

    Add files via upload

---
+++ hello.js
@@ -0,0 +1,13 @@
+//"Hello world" як сервер
+const http = require('http'); //Забезпечення присутності класу http-сервер
+const port = 8080; //Через цей порт дивимся відповідь сервера
+//Створення сервера, який на звернення лише відповідає 'Hello world'
+//Стрілочна функція описує потрібні дії сервера
+const server = http.createServer((req, res) => {
+ res.end('Hello, Pavel Minin!');
+});
+// Запуск прослуховування заданого порту сервером
+server.listen(port, () => {
+ console.log('Server listens on: http://localhost:%s', port);
+});
+//Викликом сервера є звертання в вікні браузера по адресі http://localhost:8080
